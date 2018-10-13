114701c997c8377d7a1295eca73fc129f46ba593
lexturner <44118836+lexturner@users.noreply.github.com>
2018-10-13 10:29:29 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import desc
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    body = db.Column(db.String(1200))
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.created = datetime.utcnow()
+
+    def is_valid(self):
+        if self.title and self.body and self.created:
+            return True
+        else:
+            return False
+
+@app.route('/')
+def index():
+    return redirect('/blog')
+
+@app.route('/blog')
+def blog_index():
+    blog_id = request.args.get('id')
+    blogs = Blog.query.all()
+
+    if blog_id:
+        post = Blog.query.get(blog_id)
+        blog_title = post.title
+        blog_body = post.body
+        return render_template('entry.html', title="Blog Entry #" + blog_id, blog_title=blog_title, blog_body=blog_body)
+
+    sort = request.args.get('sort')
+
+    if (sort=="newest"):
+        blogs = Blog.query.order_by(Blog.created.desc()).all()
+    elif (sort=="oldest"):
+        blogs = Blog.query.order_by(Blog.created.asc()).all()
+    else:
+        blogs = Blog.query.all()
+    return render_template('blog.html', title="Build A Blog", blogs=blogs)
+#@app.route('/', methods=['POST', 'GET'])
+#def index():
+
+#    if request.method == 'POST':
+#        task_name = request.form['task']
+#        new_task = Task(task_name)
+#        db.session.add(new_task)
+#        db.session.commit()
+
+#    if request.method == 'POST':
+#        task_body = request.form['task']
+#        new_task = Task(task_name)
+#        db.session.add(new_task)
+#        db.session.commit()
+
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
+
+
+#@app.route('/delete-task', methods=['POST'])
+#def delete_task():
+
+#    task_id = int(request.form['task-id'])
+#    task = Task.query.get(task_id)
+#    task.completed = True
+#    db.session.add(task)
+#    db.session.commit()
+
+#   return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
